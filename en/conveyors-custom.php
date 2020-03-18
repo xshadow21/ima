a954ce09e8ecafe2215620bcdabf0a04935683f7
AG <a.gorain@hotmail.com>
2020-03-17 22:51:45 -0400

    implement new page conveyor

--- en/conveyors-custom.php
+++ en/conveyors-custom.php
@@ -12,6 +12,14 @@
         <p class="emphase"><br />LM Manutentions partners with reputable conveyor distributors to meet your distribution needs.</p>
         <p>Whether it is to repair a part, modify a section or to install a new conveyor, LM Manutentions has been supporting companies since 2001 to improve their productivity.&nbsp;</p>
         <p>We adapt to your goals and your production chain; we work with companies from several industries. Here are some examples:</p>
+        <ul class="fa-ul">
+					<li><a href="/en/conveyors-processing-food">Food-processing Industry</a></li>
+					<li><a href="/en/conveyors-beverage">Beverage Industry</a></li>
+					<li><a href="/en/conveyors-packaging">Packaging</a></li>
+					<li><a href="/en/conveyors-mining-and-querries">Mines and Querries sectors</a></li>
+					<li><a href="/en/conveyors-recycling">Recycling companies</a></li>
+					<li><a href="/en/conveyors-cosmetic-pharmaceutical">Cosmetic and Pharmaceutical industries</a></li>
+        </ul>
 
         &nbsp; <br /><strong><a class="BlocOrangeButton pull-left" href="/en/request-a-quote">Request<br>a Quote</a></strong>
         <br/>
