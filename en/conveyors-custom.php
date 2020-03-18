219a6674d65682c74c31a6895834bcf18df1a8cc
AG <a.gorain@hotmail.com>
2020-03-17 22:28:55 -0400

    implement new page conveyor

--- en/conveyors-custom.php
+++ en/conveyors-custom.php
@@ -1,21 +1,17 @@
 ﻿<?php include 'header.php' ?>
+<meta name="description" content="LM Manutentions installs custom conveyors. We adapt to your needs and objectives, contact us for a free quote"/>
 
 <!-- WRAPPER -->
 <div id="wrapper"><!-- CAROUSEL -->
   <section class="container" style="margin-top: 80px;">
     <div class="row">
       <div class="col-md-6">
-        <h2 style="color: #f68b1e;"><strong>CUSTOMMMM</strong> INDUSTRY CONVEYORS</h2>
-        <h4>LM MANUTENTIONS have the solution for you!</h4>
+        <h2 style="color: #f68b1e;">Custom Conveyors</h2>
+        <h4>Versatile and quality conveyors</h4>
         <hr class="sep" />
-        <p class="emphase"><br />LM Manutentions launches his new service with conveyors specialized for bottling lines.</p>
-        <p>Our conveyors can be adapted to all types of workspace and all types of containers: empty or full bottles, glass or plastic, bottle, bottle and cans. All our conveyors meet hygiene rules and standards for the bottling industry.&nbsp;</p>
-        <p>This company has chosen for its LM Manutentions conveyors for bottling of its products:</p>
-        <ul class="fa-ul">
-          <li>Eau de source Daigneault</li>
-        </ul>
-        &nbsp; <br /><strong><a class="BlocOrangeButton pull-left" href="/en/request-a-quote">Request<br>a Quote</a></strong>
-        <div class="BlocOrangeButton pull-right">Information<br /><strong>450-226-8777</strong></div>
+        <p class="emphase"><br />LM Manutentions partners with reputable conveyor distributors to meet your distribution needs.</p>
+        <p>Whether it is to repair a part, modify a section or to install a new conveyor, LM Manutentions has been supporting companies since 2001 to improve their productivity.&nbsp;</p>
+        <p>We adapt to your goals and your production chain; we work with companies from several industries. Here are some examples:</p>
         <br/>
 				<hr class="sep" />
       </div>
@@ -25,13 +21,10 @@
           <div><img class="img-responsive" src="/images/convoyeurs/embouteillage3.jpg" alt="" /></div>
           <div><img class="img-responsive" src="/images/convoyeurs/embouteillage4.jpg" alt="" /></div>
         </div>
-        <div class="Boite" style="color: #fff;">
-          <h3>WE WORK <strong>FOR YOU</strong></h3>
-          <p>Our responsibility is to guide our clients in their choice based on their goals, their needs, their conditions of operation and their budget until the establishment of an operational, sustainable and efficient integrated handling or transport system.</p>
-        </div>
       </div>
     </div>
       <?php include 'conveyers-menu.php' ?>
+      &nbsp; <br /><strong><a class="BlocOrangeButton pull-left" href="/en/request-a-quote">Request<br>a Quote</a></strong>
   </section>
   <!-- /CAROUSEL --></div>
 <!-- /WRAPPER -->
