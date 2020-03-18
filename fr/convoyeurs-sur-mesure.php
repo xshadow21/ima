167eb09ba80fcee65a10801b3a3fd4c1024d8729
AG <a.gorain@hotmail.com>
2020-03-17 22:45:18 -0400

    implement new page conveyor fr

--- fr/convoyeurs-sur-mesure.php
+++ fr/convoyeurs-sur-mesure.php
@@ -1,24 +1,19 @@
 ﻿<?php include 'header.php' ?>
+<meta name="description" content="LM Manutentions fait l'installation de convoyeurs sur mesure. Nous nous adaptons à vos besoins, contactez-nous pour une soumission gratuite"/>
+
 <!-- WRAPPER -->
 <div id="wrapper"><!-- CAROUSEL -->
 	<section class="container" style="margin-top: 80px;">
 		<div class="row">
 			<div class="col-md-6">
-				<h2 style="color: #f68b1e;">CONVOYEURS POUR LE SECTEUR <strong>AGROALIMENTAIRE</strong></h2>
-				<h4>LM MANUTENTIONS a la solution pour vous !</h4>
+				<h2 style="color: #f68b1e;">Convoyeurs sur mesure</h2>
+				<h4>Convoyeurs polyvalents et de qualité</h4>
 				<hr class="sep" />
-				<p class="emphase"><br /> Depuis 2001, LM Manutentions installe des <strong>syst&egrave;mes de convoyage sur mesure</strong>, efficaces et durables, pour les entreprises oeuvrant dans le secteur agroalimentaire.</p>
-				<p>LM Manutentions installe des convoyeurs pour l'industrie agroalimentaire qui peuvent int&eacute;grer tous les types de conditionnement et de manutention&nbsp;: bouteilles, cartons, caisses de plastique, flacons, etc. Ils offrent une souplesse d'adaptation pour les syst&egrave;mes exigeant plusieurs produits sur une m&ecirc;me ligne de production.</p>
-				<p>Dans le d&eacute;placement et la manutention de vos produits, vous obtiendrez un d&eacute;bit efficace et s&eacute;curitaire avec un rendement rapide contribuant &agrave; l'augmentation de productivit&eacute;. Les convoyeurs install&eacute;s par LM Manutentions sont &agrave; la fois robustes et flexibles, con&ccedil;us &eacute;galement pour r&eacute;pondre aux normes de s&eacute;curit&eacute; exig&eacute;es par le secteur agroalimentaire<strong>. </strong></p>
-				<p>Notre &eacute;quipe d&rsquo;experts a une connaissance et une exp&eacute;rience approfondies des besoins des entreprises oeuvrant particuli&egrave;rement dans les domaines de l&rsquo;alimentation, des boissons douces, des boissons alcoolis&eacute;es. Qu'il s'agisse de convoyeurs &agrave; rouleaux, &agrave; courroie sanitaire ou &agrave; bande modulaire, fiez-vous &agrave; l&rsquo;&eacute;quipe<strong> LM Manutentions</strong> pour concevoir le syst&egrave;me de convoyage adapt&eacute; &agrave; vos besoins sp&eacute;cifiques de production et aux caract&eacute;ristiques de vos produits.</p>
-				<p>Ces entreprises ont choisi LM Manutentions pour leurs convoyeurs alimentaires :</p>
-				<ul class="fa-ul">
-					<li>La Pr&eacute;f&eacute;rence</li>
-					<li>Fromagerie St-Albert</li>
-					<li>Chocolat Lamontagne</li>
-				</ul>
+				<p>LM Manutentions s'associe avec des distributeurs de convoyeurs réputés afin de satisfaire vos besoins de transport de marchandises.</p>
+				<p>Que ce soit pour réparer une pièce, modifier une section ou pour l’installation d’un nouveau convoyeur, LM Manutentions accompagne les entreprises depuis 2001 pour améliorer leur productivité.</p>
+				<p>Nous nous adaptons à vos objectifs et votre chaîne de production; nous travaillons avec des entreprises de plusieurs industries. Voici des exemples :</p>
+
 				&nbsp; <br /> <a class="BlocOrangeButton pull-left" href="/fr/demander-une-soumission"> Demander une<br /><strong>soumission</strong> </a>
-				<div class="BlocOrangeButton pull-right">Information<br /><strong>450-226-8777</strong></div>
 				<br/>
 				<hr class="sep" />  
 			</div>
@@ -31,10 +26,8 @@
 					<div><img class="img-responsive" src="/images/convoyeurs/agro1.jpg" alt="" /></div>
 					<div><img class="img-responsive" src="/images/convoyeurs/agro6.jpg" alt="" /></div>
 				</div>
-				<div class="Boite" style="color: #fff;">
-					<h3>Nous travaillons <strong>pour vous</strong></h3>
-					<p>Notre responsabilit&eacute; est de guider notre client&egrave;le dans leur choix en fonction de leurs objectifs, besoins, conditions d'op&eacute;ration et de leur budget. Et ce, jusqu'&agrave; la mise en place du syst&egrave;me op&eacute;rationnel, durable et efficace.</p>
-				</div>
+
+				&nbsp; <br /> <a class="BlocOrangeButton pull-left" href="/fr/demander-une-soumission"> Demander une<br /><strong>soumission</strong> </a>
 			</div>
 		</div>
     <?php include 'conveyers-menu.php' ?>
