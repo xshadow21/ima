843077783df84033670bd506f322112134f44462
AG <a.gorain@hotmail.com>
2020-03-23 19:05:28 -0400

    rea17

--- fr/nos-realisations.php
+++ fr/nos-realisations.php
@@ -38,7 +38,39 @@
 		<div class="row">
 			<button id="firstAccordionElement" class="accordion">Agroalimentaire</button>
 			<div class="panel">
-				<p>Lorem ipsonsequat.</p>
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>
+
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner">
+					<div class="item active">
+						<img src="../images/realisation/_Convoyeur accumulation ERS52 24V .jpg">
+					</div>
+
+					<div class="item">
+						<img src="../images/realisation/_Convoyeur a courroie pour recyclage de carton .jpg">
+					</div>
+
+					<div class="item">
+						<img src="../images/realisation/_Convoyeur M525 CDLR avec emballeuse automatique.jpg">
+					</div>
+				</div>
+
+				<!-- Left and right controls -->
+				<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#myCarousel" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+					<span class="sr-only">Next</span>
+				</a>
+				</div>
 			</div>
 
 			<button class="accordion">Embouteillage</button>
@@ -85,9 +117,5 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
-
-$("document").ready(function() {
-	acc[0].trigger('click');
-});
 </script>
 <?php include 'footer.php' ?>
