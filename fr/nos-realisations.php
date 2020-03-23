d278b9c6e26b31622d59e0a8c90559d1f4bf4bd7
AG <a.gorain@hotmail.com>
2020-03-23 19:07:28 -0400

    rea18

--- fr/nos-realisations.php
+++ fr/nos-realisations.php
@@ -38,38 +38,38 @@
 		<div class="row">
 			<button id="firstAccordionElement" class="accordion">Agroalimentaire</button>
 			<div class="panel">
-			<div id="myCarousel" class="carousel slide" data-ride="carousel">
-				<!-- Indicators -->
-				<ol class="carousel-indicators">
-					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					<li data-target="#myCarousel" data-slide-to="1"></li>
-					<li data-target="#myCarousel" data-slide-to="2"></li>
-				</ol>
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+						<li data-target="#myCarousel" data-slide-to="2"></li>
+					</ol>
 
-				<!-- Wrapper for slides -->
-				<div class="carousel-inner">
-					<div class="item active">
-						<img src="../images/realisation/_Convoyeur accumulation ERS52 24V .jpg">
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="../images/realisation/_Convoyeur accumulation ERS52 24V .jpg" style="width:100%;">
+						</div>
+
+						<div class="item">
+							<img src="../images/realisation/_Convoyeur a courroie pour recyclage de carton .jpg" style="width:100%;">
+						</div>
+
+						<div class="item">
+							<img src="../images/realisation/_Convoyeur M525 CDLR avec emballeuse automatique.jpg" style="width:100%;">
+						</div>
 					</div>
 
-					<div class="item">
-						<img src="../images/realisation/_Convoyeur a courroie pour recyclage de carton .jpg">
-					</div>
-
-					<div class="item">
-						<img src="../images/realisation/_Convoyeur M525 CDLR avec emballeuse automatique.jpg">
-					</div>
-				</div>
-
-				<!-- Left and right controls -->
-				<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-					<span class="glyphicon glyphicon-chevron-left"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="right carousel-control" href="#myCarousel" data-slide="next">
-					<span class="glyphicon glyphicon-chevron-right"></span>
-					<span class="sr-only">Next</span>
-				</a>
+					<!-- Left and right controls -->
+					<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+						<span class="sr-only">Next</span>
+					</a>
 				</div>
 			</div>
 
