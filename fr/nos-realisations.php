842c264aefdf6f1d81d7c6de213ab67617a74d49
AG <a.gorain@hotmail.com>
2020-03-23 19:13:43 -0400

    rea23

--- fr/nos-realisations.php
+++ fr/nos-realisations.php
@@ -1,5 +1,7 @@
 ﻿<?php include 'header.php' ?>
 
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -41,7 +43,7 @@
 
 		<div class="row">
 			<button id="firstAccordionElement" class="accordion">Agroalimentaire</button>
-			<div class="panel">
+			<div class="panel container">
 				<div id="myCarousel" class="carousel slide" data-ride="carousel">
 					<!-- Indicators -->
 					<ol class="carousel-indicators">
