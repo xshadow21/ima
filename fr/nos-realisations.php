c5e74f83cb4e4b249110a580169538b3fd5f7d81
AG <a.gorain@hotmail.com>
2020-03-23 12:09:24 -0400

    rea5

--- fr/nos-realisations.php
+++ fr/nos-realisations.php
@@ -1,13 +1,27 @@
 ﻿<?php include 'header.php' ?>
 
-<!-- WRAPPER -->
-<div id="wrapper"><!-- CAROUSEL -->
+<div id="wrapper">
 	<section class="container" style="margin-top: 80px;">
 		<div class="row">
 			<div class="col-md-6">
 				<h2 style="color: #f68b1e;"><strong>Nos réalisations</strong></h2>
 			</div>
 		</div>
+
+		<ul class="collapsible">
+			<li>
+				<div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+				<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+			</li>
+			<li>
+				<div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+				<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+			</li>
+			<li>
+				<div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+				<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+			</li>
+		</ul>
 
 		<div class="row">
 			Agroalimentaire
@@ -34,6 +48,7 @@
 		</div>
     <?php include 'conveyers-menu.php' ?>
 	</section>
-	<!-- /CAROUSEL --></div>
-<!-- /WRAPPER -->
+</div>
+
+<script type="text/javascript" src="../libs/materialize/js/materialize.min.js"></script>
 <?php include 'footer.php' ?>
